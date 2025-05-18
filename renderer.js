c45f490dcf30ff4cb764647f74ed05b75ee3396d
junathy <pthy19@gmail.com>
2025-05-17 20:31:02 -0700

    Add error handling with try-catch in searchMovie

--- renderer.js
+++ renderer.js
@@ -1,24 +1,33 @@
 const apiKey = '617f4607';
 
-
-async function searchMovie(title) {
-    const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(title)}`);
-    const data = await response.json();
-
-    if (data.Response === "True") {
-        console.log(data);
-    } else {
-        console.error('Movie not found');
-    }
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.getElementById('search-btn');
-    
-    searchButton.addEventListener('click', () => {
+
+    searchButton.addEventListener('click', async () => {
         const title = document.getElementById('movie-input').value.trim();
         if (title) {
             searchMovie(title);
+        } else {
+            alert("Please enter a movie title.");
         }
     });
 });
+
+async function searchMovie(title) {
+    try {
+        const url = `https://www.omdbapi.com/?apikey=${apiKey}&t=${encodeURIComponent(title)}`
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.Response === "True") {
+            console.log(data);
+        } else {
+            console.error('Movie not found');
+            alert('Movie not found.');
+        }
+    } catch (error) {
+        console.error('Error fetching movie data:', error);
+        alert('Failed to fetch movie data.')
+    }
+}
+
